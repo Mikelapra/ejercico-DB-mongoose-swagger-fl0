f95187800d49b2402fc7f57d11fd14599cdc5578
unknown <apraiz.mikel@gmail.com>
2024-02-20 00:50:30 +0100

    sprint 16 entregable 1 terminado1

--- docs/tasks.js
+++ docs/tasks.js
@@ -1,105 +1,110 @@
 module.exports = {
-    paths: {
-      "/create": {
-        post: {
-          tags: {
-            Tasks: "Create a user",
-          },
-          description: "Create User",
-          operationId: "createUser",
-          parameters: [],
-          requestBody: {
-            content: {
-              "application/json": {
-                schema: {
-                  $ref: "#/components/schemas/User",
-                },
+  paths: {
+    "/create": {
+      post: {
+        tags: {
+          task: "Create a task",
+        },
+        description: "Create task",
+        operationId: "createUser",
+        parameters: [],
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/User",
               },
             },
           },
-          responses: {
-            },
-            500: {
-              description: "There was a problem trying to create a task",
-            },
+        },
+        responses: {
+          500: {
+            description: "Server error",
           },
-        }
-      },
-      "/": {
-        get: {
-            tags: {
-              Tasks: "Get task",
-            },
-            description: "Get Task",
-            operationId: "createUser",
-            parameters: [],
-            requestBody: {
-              content: {
-                "application/json": {
-                  schema: {
-                    $ref: "#/components/schemas/User",
-                  },
-                },
-              },
-            },
-            responses: {
-              201: {
-                description: "There was a problem trying to get a task"
-              },
-            },
-          }
-      },
-       "/id/:_id": {
-        put: {
-            tags: {
-              Tasks: "Update task",
-            },
-            description: "Update a task",
-            operationId: "Task",
-            parameters: [],
-            requestBody: {
-              content: {
-                "application/json": {
-                  schema: {
-                    $ref: "#/components/schemas/User",
-                  },
-                },
-              },
-            },
-            responses: {
-              201: {
-                description: "User created successfully",
-              },
-              500: {
-                description: "Server error",
-              },
-            },
-          }
-      },
-      "/id/:_id": {
-        delete: {
-            tags: {
-              Tasks: "Delete task",
-            },
-            description: "Delete task",
-            operationId: "Task",
-            parameters: [],
-            requestBody: {
-              content: {
-                "application/json": {
-                  schema: {
-                    $ref: "#/components/schemas/User",
-                  },
-                },
-              },
-            },
-            responses: {
-              500: {
-                description: "There was a problem trying to delete a task",
-              },
-            },
-          }
+        },
       }
-    }
-  
-  
+    },"/": {
+      get: {
+        tags: {
+          task: "get a task",
+        },
+        description: "get task",
+        operationId: "createUser",
+        parameters: [],
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/User",
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: "User created successfully",
+          },
+          500: {
+            description: "Server error",
+          },
+        },
+      }
+    },"/id/:_id": {
+      put: {
+        tags: {
+          task: "update a task title",
+        },
+        description: "update a task title",
+        operationId: "createUser",
+        parameters: [],
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/User",
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: "User created successfully",
+          },
+          500: {
+            description: "Server error",
+          },
+        },
+      }
+    },"/id/:_id": {
+      delete: {
+        tags: {
+          task: "delete a task",
+        },
+        description: "delete a task",
+        operationId: "createUser",
+        parameters: [],
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                $ref: "#/components/schemas/User",
+              },
+            },
+          },
+        },
+        responses: {
+          500: {
+            description: "There was a problem trying to delete a task",
+          },
+        },
+      }
+    },
+
+
+
+
+
+
+
+  },
+}
